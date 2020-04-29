1780d36cf2ce65cb9a404738a770cf7934a726bc
oliver.bannister <oliver.bannister@roxorgaming.co.uk>
2020-04-29 20:22:27 +0100

    Full stack app complete

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import { getMetrics } from './getMetrics'
+import Button from './Button';
 
 class App extends React.Component {
 
@@ -13,19 +14,27 @@ class App extends React.Component {
   }
 
   componentDidMount() {
+    this.retrieveMetrics();
+  }
+
+  clearState = () => {
+    this.setState({ metrics: {} });
+  }
+
+  retrieveMetrics = () => {
     getMetrics().then((metrics) => {
       this.setState({ metrics })
     });
   }
+
   render() {
     return (
       <div className="App">
-        <header>Website metrics</header>
-        <h1>
+        <Button height="50px" buttonColor="red" hoverColor="blue" onClick={this.clearState} >Click me to clear metrics</Button>
+        <Button height="50px" buttonColor="green" hoverColor="blue" onClick={this.retrieveMetrics} >Click me to retrieve metrics</Button>
           <ul>
-            {Object.entries(this.state.metrics).map(metric =><li>{`${metric[0]} : ${metric[1]}`}</li>)}
+          {Object.entries(this.state.metrics).map((metric, index) =><li key={index}>{`${metric[0]} : ${metric[1]}`}</li>)}
           </ul>
-        </h1>
        </div>
        );
       }
--- src/Button.js
+++ src/Button.js
@@ -4,13 +4,16 @@ const Button = styled.button`
     font-family: sans-serif;
     font-size: 1.3rem;
     border: none;
-    border-radius: 5px;
-    background-color: blue;
-    background: ${(props) => props.theme.primary};
+    border-radius: 15px;
+    background-color: ${(props) => props.buttonColor};
+    width: ${(props) => props.width};
+    height: ${(props) => props.height};
     color: #fff;
+    padding: 5px;
+    margin: 10px;
     &:hover
     {
-        background-color: red;
+        background-color: ${(props) => props.hoverColor};
     }
 `
 export default Button;
